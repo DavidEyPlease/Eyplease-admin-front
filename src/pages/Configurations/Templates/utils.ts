6b7931206612ff215ec61d606810caeafa594612
Luis Alejandro Henriquez <luis.ing.henriquez@gmail.com>
2026-03-23 12:16:48 -0500

    Update utils.ts

--- src/pages/Configurations/Templates/utils.ts
+++ src/pages/Configurations/Templates/utils.ts
@@ -16,16 +16,16 @@ export const FORM_DEFAULT_VALUES = {
 
 export const TEMPLATE_GROUPS = [
     { label: 'Reportes', value: 'reports' },
-    { label: 'Cumpleaños', value: 'birthdays' },
-    { label: 'Estrellas', value: 'stars' },
-    { label: 'Cuador de honor', value: 'honor_board' },
-    { label: 'Nuevos inicios', value: 'new_beginnings' },
-    { label: 'Camino al éxito', value: 'path_to_success' },
-    { label: 'Aniversarios', value: 'anniversaries' },
-    { label: 'Directora en calificación', value: 'director_in_rating' },
-    { label: 'Corte de ventas', value: 'sales_cut' },
-    { label: 'Corte de iniciación', value: 'initiation_cut' },
-    { label: 'Unit club', value: 'unit_club' },
+    { label: 'Ordenantes del mes (Publicacines)', value: 'early' },
+    // { label: 'Estrellas', value: 'stars' },
+    // { label: 'Cuador de honor', value: 'honor_board' },
+    // { label: 'Nuevos inicios', value: 'new_beginnings' },
+    // { label: 'Camino al éxito', value: 'path_to_success' },
+    // { label: 'Aniversarios', value: 'anniversaries' },
+    // { label: 'Directora en calificación', value: 'director_in_rating' },
+    // { label: 'Corte de ventas', value: 'sales_cut' },
+    // { label: 'Corte de iniciación', value: 'initiation_cut' },
+    // { label: 'Unit club', value: 'unit_club' },
 ]
 
 export const RENDER_ASSETS_TYPES = [
